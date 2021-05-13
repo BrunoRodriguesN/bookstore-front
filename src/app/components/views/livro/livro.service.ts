c961ae69c706cb4b1be40f1f249b77d50600bdfb
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-05-13 12:41:15 -0300

    delete para livro implementado

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -36,6 +36,12 @@ export class LivroService {
 
     }
 
+    delete(id: String):Observable<void> {
+        const url = `${this.baseUrl}/livros/${id}`
+        return this.http.delete<void>(url)
+
+    }
+
     mensagem(str: String): void {
         this._snack.open(`${str}`, `OK`, {
             horizontalPosition: 'end',
